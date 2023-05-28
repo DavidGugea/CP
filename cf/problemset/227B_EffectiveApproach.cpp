fe91dd3605182e7cc8b0ad5efc6645a21482c9cb
David Gugea <davidgugea@yahoo.com>
2023-05-28 19:28:26 +0200

    cf

--- cf/problemset/227B_EffectiveApproach.cpp
+++ cf/problemset/227B_EffectiveApproach.cpp
@@ -15,5 +15,31 @@ int main() {
 		v.push_back(val);
 	}
 
+	int vasya = 0; // 1 -> n
+	int petya = 0; // n -> 1
+
+	int m;
+	cin >> m;
+
+	// 1 2 3 4 5
+	// 0 1 2 3 4
+
+	for (int i = 0 ; i < m ; ++i) {
+		int search_value; cin >> search_value;
+		int count = 0;
+
+		for (int j = 0 ; j < n; ++j) {
+			vasya++;
+			count++;
+
+			if (v[j] == search_value) {
+				petya += n - count + 1;
+				break;
+			}
+		}
+	}
+
+	cout << vasya << " " << petya << "\n";
+
 	return 0;
 }
